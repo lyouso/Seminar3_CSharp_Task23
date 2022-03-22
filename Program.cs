195bdfe4b39c8e63e722439556462c36fad0b65f
Liubov K <lyouso@yandex.ru>
2022-03-22 15:21:52 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    int cube_number = i*i*i;
+    Console.WriteLine("Куб числа " + i + " --> " + cube_number);
+}
